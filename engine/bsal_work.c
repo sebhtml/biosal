251c0742a74232c6392b8369ecfebc957a029e1e
Sébastien Boisvert <boisvert@anl.gov>
2014-05-23 17:30:03 -0500

    remove receive from work
    The receive function is included in the actor structure.
    
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- engine/bsal_work.c
+++ engine/bsal_work.c
@@ -3,10 +3,9 @@
 
 #include <stdlib.h>
 
-void bsal_work_construct(struct bsal_work *work, bsal_actor_receive_fn_t receive,
-                struct bsal_actor *actor, struct bsal_message *message)
+void bsal_work_construct(struct bsal_work *work, struct bsal_actor *actor,
+                struct bsal_message *message)
 {
-    work->receive = receive;
     work->actor = actor;
     work->message = message;
     work->completed = 0;
@@ -15,7 +14,6 @@ void bsal_work_construct(struct bsal_work *work, bsal_actor_receive_fn_t receive
 
 void bsal_work_destruct(struct bsal_work *work)
 {
-    work->receive = NULL;
     work->actor = NULL;
     work->message = NULL;
     work->completed = -1;
--- engine/bsal_work.h
+++ engine/bsal_work.h
@@ -4,17 +4,15 @@
 
 #include "bsal_actor.h"
 
-
 struct bsal_work {
-    bsal_actor_receive_fn_t receive;
     struct bsal_actor *actor;
     struct bsal_message *message;
     int completed;
     int started;
 };
 
-void bsal_work_construct(struct bsal_work *work, bsal_actor_receive_fn_t receive,
-                struct bsal_actor *actor, struct bsal_message *message);
+void bsal_work_construct(struct bsal_work *work, struct bsal_actor *actor,
+                struct bsal_message *message);
 void bsal_work_destruct(struct bsal_work *work);
 
 #endif
