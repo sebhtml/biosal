6d864a5599769a9b30265247afb525083eb63c12
Sébastien Boisvert <boisvert@anl.gov>
2014-05-22 21:44:13 -0500

    fix new lines in test
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- test/interface.c
+++ test/interface.c
@@ -11,12 +11,10 @@ int main(int argc, char **argv)
 	struct bsal_node node;
 
     mock_actor_construct(&mock_actor);
-
     bsal_actor_construct(&actor, &mock_actor, &mock_actor_vtable);
-
 	bsal_node_construct(&node, 1,  1,  1);
-	bsal_node_spawn_actor(&node, &actor);
 
+	bsal_node_spawn_actor(&node, &actor);
     bsal_node_start(&node);
 
     bsal_node_destruct(&node);
