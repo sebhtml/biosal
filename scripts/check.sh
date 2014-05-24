c64f449a3453a0af3a603196a94c7b94b10951c2
Sébastien Boisvert <boisvert@anl.gov>
2014-05-24 18:56:07 -0500

    add some tests in the style checks
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 make clean
-grep "if("  * -R|grep -v git
-grep "for(" * -R|grep -v git
-grep " $" * -R|grep -v git
-grep "	" * -R|grep -v Makefile|grep -v git
+grep -n "if  ("  * -R|grep -v git
+grep -n "if("  * -R|grep -v git
+grep -n "for(" * -R|grep -v git
+grep -n "for  (" * -R|grep -v git
+grep -n "  =" * -R|grep -v git
+grep -n "=  " * -R|grep -v git
+grep -n " $" * -R|grep -v git
+grep -n "	" * -R|grep -v Makefile|grep -v git
