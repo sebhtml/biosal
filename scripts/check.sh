cdde0ee7c1ae0209dba28ddb01b99e7b869ed268
Sébastien Boisvert <boisvert@anl.gov>
2014-05-25 18:02:54 -0500

    improve script that checks for strange things in code
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 
+function run_checks() {
+    grep -n "struct  "  * -R|grep -v git
+    grep -n "enum  "  * -R|grep -v git
+    grep -n "if  ("  * -R|grep -v git
+    grep -n "if("  * -R|grep -v git
+    grep -n "for(" * -R|grep -v git
+    grep -n "for  (" * -R|grep -v git
+    grep -n " -> " * -R|grep -v git | grep -v printf
+    grep -n "  =" * -R|grep -v git
+    grep -n "=  " * -R|grep -v git
+    grep -n " $" * -R|grep -v git
+    grep -n "	" * -R|grep -v Makefile|grep -v git
+}
+
+clear
+
+echo "=== Quality Assurance ($(date)) ==="
+
+echo "User: $(whoami)"
+echo "Host: $(hostname)"
+
+git branch
+echo ""
+git diff --stat energy
+echo ""
+git status
+echo ""
+
 make clean
-grep -n "struct  "  * -R|grep -v git
-grep -n "enum  "  * -R|grep -v git
-grep -n "if  ("  * -R|grep -v git
-grep -n "if("  * -R|grep -v git
-grep -n "for(" * -R|grep -v git
-grep -n "for  (" * -R|grep -v git
-grep -n " -> " * -R|grep -v git | grep -v printf
-grep -n "  =" * -R|grep -v git
-grep -n "=  " * -R|grep -v git
-grep -n " $" * -R|grep -v git
-grep -n "	" * -R|grep -v Makefile|grep -v git
+
+echo ""
+
+echo "Detected issues: $(run_checks|wc -l)"
+
+run_checks
+
+echo "Thank you."
