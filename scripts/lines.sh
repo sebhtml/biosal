6a06adb96aa9f087cb8906c9c5f5989b6a56e9ed
Sébastien Boisvert <boisvert@anl.gov>
2014-05-31 09:03:24 -0500

    only show the first 30 lines when counting lines
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- scripts/lines.sh
+++ scripts/lines.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-wc -l $((find .|grep '\.c'$;find .|grep '\.h'$)|grep -v '\.git')|sort -r -n
+wc -l $((find .|grep '\.c'$;find .|grep '\.h'$)|grep -v '\.git')|sort -r -n | head -n 30
+echo "..."
