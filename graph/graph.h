f16d45524500d2a940b1f7315c9a8ddd5e234579
Sébastien Boisvert <boisvert@anl.gov>
2014-06-16 15:24:06 +0000

    remove graph header because this will be an actor too
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- graph/graph.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef BSAL_GRAPH_H
-#define BSAL_GRAPH_H
-
-struct bsal_graph_actor {
-    int mock;
-};
-
-#endif
--- scripts/clang.sh
+++ scripts/clang.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#-Wall -std=c99 
+#-Wall -std=c99
 make clean
 make -j 1 CC=clang CFLAGS="-I. -I/usr/include/mpich2 -D_POSIX_C_SOURCE=200112L -Wall --analyze -Werror -pedantic -x c -std=c99"
