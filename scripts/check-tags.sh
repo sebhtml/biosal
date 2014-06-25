adf6eabf57ef3cab0c9e4910b05c8f52e6bdd22f
Sébastien Boisvert <boisvert@anl.gov>
2014-06-25 07:46:58 -0500

    fix MPI problem on Beagle
    Link: https://github.com/GeneAssembly/biosal/issues/283
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- scripts/check-tags.sh
+++ scripts/check-tags.sh
@@ -6,3 +6,22 @@ grep 0x0000 * -R|grep -v check-tags.sh|awk '{print $2}'|sort|uniq -c|awk '{print
 echo -n "Tag values :"
 grep 0x0000 * -R|grep -v check-tags.sh|awk '{print $3}'|sort|uniq -c|awk '{print $1}'|sort -n|uniq -c
 grep 0x0000 * -R|grep -v check-tags.sh|awk '{print $3}'|sort|uniq -c|grep -v " 1 "
+
+min=0
+max=32767
+
+for tag in $(grep 0x * -R|grep -v ')'|grep -v table | grep -v SCRIPT|grep -v check-tags.sh|awk '{print $3}'|grep 0x)
+do
+    #echo $tag
+    if [[ $tag -lt $min ]]
+    then
+        echo "Error $tag < $min"
+    fi
+
+    if [[ $tag -gt $max ]]
+    then
+        echo "Error $tag > $max"
+
+        grep $tag * -R
+    fi
+done
