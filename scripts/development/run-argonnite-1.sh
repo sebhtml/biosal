f1a47effc1d025d93b6aa64c3e2761e4b75f865d
Sébastien Boisvert <boisvert@anl.gov>
2014-07-14 20:43:22 +0000

    use 28 threads instead of 32 since the machine is shared
    Link: https://github.com/GeneAssembly/biosal/issues/404
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- scripts/development/run-argonnite-1.sh
+++ scripts/development/run-argonnite-1.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-mpiexec -n 4 applications/argonnite -k 43 -threads-per-node 8 ~/dropbox/GPIC.1424-1.1371.fastq
+# change this from 8 threads to 7 threads because the machine is shared.
+mpiexec -n 4 applications/argonnite -k 43 -threads-per-node 7 ~/dropbox/GPIC.1424-1.1371.fastq
