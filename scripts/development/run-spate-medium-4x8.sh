83456a28b23cc9fd87fef7ee5bcb270980c870c2
Sébastien Boisvert <boisvert@anl.gov>
2014-08-03 12:19:59 -0400

    don't use -print-load
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- scripts/development/run-spate-medium-4x8.sh
+++ scripts/development/run-spate-medium-4x8.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mpiexec -n 4 applications/spate_metagenome_assembler/spate -print-load -k 43 -threads-per-node 8 ~/dropbox/medium.fastq
+mpiexec -n 4 applications/spate_metagenome_assembler/spate -k 43 -threads-per-node 8 ~/dropbox/medium.fastq
