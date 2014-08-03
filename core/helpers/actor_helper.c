f761d2346f86a1245f90808e6968dbf7d636760d
Sébastien Boisvert <boisvert@anl.gov>
2014-08-03 12:20:17 -0400

    hide actor access violations
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- core/helpers/actor_helper.c
+++ core/helpers/actor_helper.c
@@ -489,8 +489,10 @@ void bsal_actor_helper_ask_to_stop(struct bsal_actor *actor, struct bsal_message
     int child;
 
     if (source != name && source != supervisor) {
+#ifdef BSAL_ACTOR_HELPER_DEBUG_STOP
         printf("actor/%d: permission denied, will not stop (source: %d, name: %d, supervisor: %d\n",
                         bsal_actor_name(actor), source, name, supervisor);
+#endif
         return;
     }
 
