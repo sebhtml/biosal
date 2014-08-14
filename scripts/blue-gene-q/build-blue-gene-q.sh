d030c49488f3946d2f9513f77c5fa84426e7140d
Sébastien Boisvert <boisvert@anl.gov>
2014-08-14 14:24:54 +0000

    fix build command on Blue Gene/Q
    Link: https://github.com/GeneAssembly/biosal/issues/526
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- scripts/blue-gene-q/build-blue-gene-q.sh
+++ scripts/blue-gene-q/build-blue-gene-q.sh
@@ -4,7 +4,14 @@
 
 # -O5 to enable -ipa
 # -s to strip symbols
+
+CFLAGS="-I. -g -O3 -qmaxmem=-1 -qarch=qp -qtune=qp -DBSAL_DEBUGGER_ENABLE_ASSERT "
+CFLAGS="$CFLAGS -L/soft/libraries/alcf/current/xl/ZLIB/lib -I/soft/libraries/alcf/current/xl/ZLIB/include"
+
+#echo $CFLAGS
+#exit
+
 make clean
-make -j 8 CFLAGS="-I. -g -O3 -qmaxmem=-1 -qarch=qp -qtune=qp -DBSAL_DEBUGGER_ENABLE_ASSERT" \
+make -j 8 CFLAGS="$CFLAGS" \
     applications/argonnite_kmer_counter/argonnite \
     applications/spate_metagenome_assembler/spate
