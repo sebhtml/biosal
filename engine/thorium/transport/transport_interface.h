0b61533c29256e9aad61108b12c188d7926eb47e
Sébastien Boisvert <boisvert@anl.gov>
2014-08-26 15:52:14 -0400

    add return values for transport interface
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- engine/thorium/transport/transport_interface.h
+++ engine/thorium/transport/transport_interface.h
@@ -25,16 +25,21 @@ struct thorium_transport_interface {
 
     /*
      * Send a message. This should be non-blocking.
+     * \returns 1 if the message was sent. 0 Otherwise.
      */
     int (*send)(struct thorium_transport *self, struct thorium_message *message);
 
     /*
      * Receive a message. This should be non-blocking.
+     *
+     * \returns 1 if a message was received. 0 otherwise.
      */
     int (*receive)(struct thorium_transport *self, struct thorium_message *message);
 
     /*
      * Test to check if there are worker buffers to recycle.
+     * \returns 1 if worker_buffer has been updated with the worker identifier
+     * and buffer pointer.
      */
     int (*test)(struct thorium_transport *self, struct thorium_worker_buffer *worker_buffer);
 };
