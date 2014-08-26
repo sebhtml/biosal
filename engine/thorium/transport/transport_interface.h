1c536fcf05500f3656471fcd1b75e00383f2b2e3
Sébastien Boisvert <boisvert@anl.gov>
2014-08-26 15:48:03 -0400

    document the interface for transport
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- engine/thorium/transport/transport_interface.h
+++ engine/thorium/transport/transport_interface.h
@@ -13,12 +13,29 @@ struct thorium_transport_interface {
     char *name;
     int size;
 
+    /*
+     * Initialize the concrete transport
+     */
     void (*init)(struct thorium_transport *self, int *argc, char ***argv);
+
+    /*
+     * Destroy the concrete transport
+     */
     void (*destroy)(struct thorium_transport *self);
 
+    /*
+     * Send a message. This should be non-blocking.
+     */
     int (*send)(struct thorium_transport *self, struct thorium_message *message);
+
+    /*
+     * Receive a message. This should be non-blocking.
+     */
     int (*receive)(struct thorium_transport *self, struct thorium_message *message);
 
+    /*
+     * Test to check if there are worker buffers to recycle.
+     */
     int (*test)(struct thorium_transport *self, struct thorium_worker_buffer *worker_buffer);
 };
 
