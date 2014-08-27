694343e2a4defc664d6773adce6fa82e3d2ec431
Sébastien Boisvert <boisvert@anl.gov>
2014-08-27 13:23:36 -0500

    use Cray aprun core specialization
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- scripts/cray/job-submission-example.sh
+++ scripts/cray/job-submission-example.sh
@@ -6,20 +6,17 @@
 
 cd $PBS_O_WORKDIR
 
-# http://www.training.prace-ri.eu/uploads/tx_pracetmo/MPI-environment-variables.pdf
-export MPICH_GNI_MAX_EAGER_MSG_SIZE=16384
-export MPICH_GNI_NUM_BUFS=128
+# Cray XE6: http://www.training.prace-ri.eu/uploads/tx_pracetmo/comms.pdf
+# Cray XC: http://www.training.prace-ri.eu/uploads/tx_pracetmo/MPI-environment-variables.pdf
 
 # turn on async progress
 # this is similar to PAMID_THREAD_MULTIPLE=1 for MPI on Blue Gene/Q (PAMID)
 export MPICH_NEMESIS_ASYNC_PROGRESS=1
 export MPICH_MAX_THREAD_SAFETY=multiple
-export MPICH_GNI_USE_UNASSIGNED_CPUS=enabled
 
-# disable dynamic connections because we are using all-to-all anyway
-export MPICH_GNI_DYNAMIC_CONN=disabled
-
-aprun -n 256 -N 1 -d 24 \
-              spate -threads-per-node 24 -print-load \
+# -r 1 tells aprun to use 1 core for specialization
+# The nemesis engine in MPI will then be able to use that.
+aprun -n 256 -N 1 -d 23 -r 1 \
+              spate -threads-per-node 23 -print-load \
               -k 43 Iowa_Continuous_Corn/*.fastq -o spate-work-stealing-607-256x24-10 > spate-work-stealing-607-256x24-10.stdout
 
