3977b512c1e5ac2cb4ecae55f61ab86a54c65a58
Sébastien Boisvert <boisvert@anl.gov>
2014-08-27 13:23:14 +0000

    add a job submission example for Blue Gene Q
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

---
+++ scripts/blue-gene-q/job-submission-example.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+qsub \
+ --env PAMID_THREAD_MULTIPLE=1 \
+ -A CompBIO \
+ -n 2048 \
+ -t 00:30:00 \
+ -O strong-scaling-569-spate-thorium-cetus-2048x16-200 \
+ --mode c1 \
+ spate -print-load -threads-per-node 16 -k 43 Iowa_Continuous_Corn/*.fastq -o strong-scaling-569-spate-thorium-cetus-2048x16-200
