0e27277a583ffc9d6d31b821601521ad594517f1
Sébastien Boisvert <boisvert@anl.gov>
2014-10-01 10:56:55 -0500

    cray: add THORIUM_DEBUG with GNU
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- scripts/Cray_XE6/build-gnu.sh
+++ scripts/Cray_XE6/build-gnu.sh
@@ -3,5 +3,5 @@
 module swap PrgEnv-cray/4.2.24 PrgEnv-gnu/4.2.24
 
 make clean
-make CC=cc -j 4 applications/argonnite_kmer_counter/argonnite \
+make CC=cc -j 4 applications/argonnite_kmer_counter/argonnite CONFIG_FLAGS="-DTHORIUM_DEBUG" \
         applications/spate_metagenome_assembler/spate
