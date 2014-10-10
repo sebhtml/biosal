09dde2739bbff2ca5462c4ec29c513fa749c9453
Sébastien Boisvert <boisvert@anl.gov>
2014-10-10 17:50:00 -0500

    cray: add script to build with PGI
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

---
+++ scripts/Cray_XE6/build-with-pgi.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+module swap PrgEnv-cray/4.2.24 PrgEnv-pgi/4.2.24
+
+make clean
+make CC=cc -j 4 applications/argonnite_kmer_counter/argonnite CONFIG_FLAGS="-DTHORIUM_DEBUG" \
+        applications/spate_metagenome_assembler/spate
