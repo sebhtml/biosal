b2166736c96a1ffd9d37baa5683e4d25ccfcb40a
Sébastien Boisvert <boisvert@anl.gov>
2014-10-10 19:16:39 +0000

    IBM: add a script to build with clang
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- scripts/IBM_Blue_Gene_Q/build-spate-clang.sh
+++ scripts/IBM_Blue_Gene_Q/build-spate-clang.sh
@@ -8,9 +8,6 @@ CFLAGS="-I. -g -O3 -DTHORIUM_DEBUG "
 CFLAGS="$CFLAGS -I/soft/libraries/alcf/current/xl/ZLIB/include"
 LDFLAGS="-L/soft/libraries/alcf/current/xl/ZLIB/lib -lm -lz"
 
-#echo $CFLAGS
-#exit
-
 make clean
 make -j 8 CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" \
     applications/spate_metagenome_assembler/spate
