787731b5acf012aa956760214f4a267a0b82d8b6
Sébastien Boisvert <boisvert@anl.gov>
2014-10-24 14:10:23 -0400

    scripts: remove trailing spaces in cray build script
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- scripts/Cray_XE6/build-gnu.sh
+++ scripts/Cray_XE6/build-gnu.sh
@@ -9,17 +9,17 @@ module purge
 # if only the minimal set of modules is loaded, MPICH throws this one:
 # Assertion failed in file /notbackedup/tmp/ulib/mpt/nightly/6.0/081313/mpich2/src/mpi/datatype/get_count.c at line 34: size >= 0 && status->count >= 0
 
-module load modules/3.2.6.7                                                           
-module load nodestat/2.2-1.0402.44853.2.1.gem                                         
-module load sdb/1.0-1.0402.44037.2.31.gem                                             
-module load MySQL/5.0.64-1.0000.7096.23.1                                             
-module load lustre-cray_gem_s/1.8.6_2.6.32.59_0.7.1_1.0402.7200.5.1-1.0402.44933.9.1  
-module load udreg/2.3.2-1.0402.7311.2.1.gem                                           
-module load ugni/5.0-1.0402.7128.7.6.gem                                              
-module load gni-headers/2.1-1.0402.7082.6.2.gem                                       
-module load dmapp/4.0.1-1.0402.7439.5.1.gem                                           
-module load xpmem/0.1-2.0402.44035.2.1.gem                                            
-module load hss-llm/7.0.0                                                             
+module load modules/3.2.6.7
+module load nodestat/2.2-1.0402.44853.2.1.gem
+module load sdb/1.0-1.0402.44037.2.31.gem
+module load MySQL/5.0.64-1.0000.7096.23.1
+module load lustre-cray_gem_s/1.8.6_2.6.32.59_0.7.1_1.0402.7200.5.1-1.0402.44933.9.1
+module load udreg/2.3.2-1.0402.7311.2.1.gem
+module load ugni/5.0-1.0402.7128.7.6.gem
+module load gni-headers/2.1-1.0402.7082.6.2.gem
+module load dmapp/4.0.1-1.0402.7439.5.1.gem
+module load xpmem/0.1-2.0402.44035.2.1.gem
+module load hss-llm/7.0.0
 module load Base-opts/1.0.2-1.0402.42088.1.3.gem
 module load craype-network-gemini
 module load PrgEnv-gnu/4.2.24
