260ad46f9977591a34103cddd28224d92749fa7b
Sébastien Boisvert <boisvert@anl.gov>
2014-10-24 21:06:22 -0400

    scripts: add a LTTng script to trace nodes
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

---
+++ scripts/lttng/trace-node_enter+exit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+duration=5
+
+lttng create
+lttng enable-event -u thorium_node:tick_enter,thorium_node:tick_exit --filter " node == 1"
+echo "Tracing for $duration seconds"
+lttng start
+sleep $duration
+lttng stop
+lttng view > node.trace
+wc -l node.trace
+lttng destroy
+
