2bab0ff9687fd6592ef1e5178c0a070b7ccfed61
Sébastien Boisvert <boisvert@anl.gov>
2014-10-30 16:23:39 -0500

    scripts: add authorship in build script
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#
+# Sébastien's script
+
 CFLAGS="-rdynamic -O3 -march=x86-64 -g -std=c99 -Wall -Wextra -pedantic -I. -Wno-unused-parameter -D_POSIX_C_SOURCE=200112L -Werror -DTHORIUM_DEBUG"
 clear
 echo "CFLAGS: $CFLAGS"
