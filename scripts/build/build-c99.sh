9c8a387d740f46c9458293481d12abbc67748032
George K. Thiruvathukal <gkt@cs.luc.edu>
2014-10-30 14:32:11 -0500

    scripts: remove -rdynamic option from George's build script
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- scripts/build/build-c99.sh
+++ scripts/build/build-c99.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-CFLAGS="-rdynamic -O3 -march=x86-64 -g -std=c99 -Wall -I. -D_POSIX_C_SOURCE=200112L -DTHORIUM_DEBUG"
+CFLAGS="-O3 -march=x86-64 -g -std=c99 -Wall -I. -D_POSIX_C_SOURCE=200112L -DTHORIUM_DEBUG"
 clear
 echo "CFLAGS: $CFLAGS"
 
