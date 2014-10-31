bc0a23dc94adddb3833552e980a6f2137bfd8268
Sébastien Boisvert <boisvert@anl.gov>
2014-10-31 15:33:11 -0400

    thorium: enable binomial tree delivery route
    Link: https://github.com/GeneAssembly/biosal/issues/781
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- engine/thorium/modules/send_helpers.c
+++ engine/thorium/modules/send_helpers.c
@@ -23,8 +23,8 @@
  * Enable the binomial tree subsystem.
  */
 /*
-#define USE_BINOMIAL_TREE
 */
+#define USE_BINOMIAL_TREE
 
 void thorium_actor_send_vector(struct thorium_actor *actor, int destination,
                 int tag, struct core_vector *vector)
