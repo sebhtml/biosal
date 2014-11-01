sebhtml/biosal

commit ab22c4fa5e89eb876013b005b75422638f5d12fd

thorium: increase actor-level throughput by +75%
The ring that is currently used for recycling messages is a bottleneck
(both for the number of CPU cycles in the main node thread and in the
contention and/or cache misses caused by the spinlock on this ring).

By using a fast delivery path (worker-to-worker) for recycled messages,
the level of contention is diminished significantly. Also, the burden of
recycling is moved from the node thread to worker threads (distributed).

Performance indicator:
(actor-level throughpout increase: +75%)

Configuration:
4 nodes, 28 worker threads (4 * 7), 2800 actors (28 * 100)
Total sent message count: 112000000 (2800 * 40000)

Before:
Run 1: Actor messaging rate: 235.919687 messages / second
Run 2: Actor messaging rate: 235.187357 messages / second
Run 3: Actor messaging rate: 205.375243 messages / second
Run 4: Actor messaging rate: 179.353341 messages / second

After:
Run 1: Actor messaging rate: 347.582644 messages / second
Run 2: Actor messaging rate: 354.333163 messages / second
Run 3: Actor messaging rate: 343.403769 messages / second
Run 4: Actor messaging rate: 340.239907 messages / second

LTTng-UST was very useful in tracking the bottleneck.

Link: https://github.com/GeneAssembly/biosal/issues/751
Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

engine/thorium/worker.c
@@ -135,6 +135,11 @@ static int thorium_worker_publish_message(struct thorium_worker *self, struct th
 static int thorium_worker_enqueue_message_for_multiplexer(struct thorium_worker *self,
                 struct thorium_message *message);
 
+static int thorium_worker_enqueue_message_for_triage(struct thorium_worker *worker, struct thorium_message *message);
+/*
+static int thorium_worker_dequeue_message_for_triage(struct thorium_worker *worker, struct thorium_message *message);
+*/
+
 void thorium_worker_init(struct thorium_worker *worker, int name, struct thorium_node *node)
 {
     int capacity;
@@ -202,6 +207,7 @@ void thorium_worker_init(struct thorium_worker *worker, int name, struct thorium
     injected_buffer_ring_size = capacity;
     core_fast_ring_init(&worker->input_clean_outbound_buffer_ring,
                     injected_buffer_ring_size, sizeof(void *));
+    core_fast_ring_use_multiple_producers(&worker->input_clean_outbound_buffer_ring);
 
     worker->output_message_ring_for_triage = NULL;
 
@@ -844,6 +850,13 @@ int thorium_worker_dequeue_actor(struct thorium_worker *worker, struct thorium_a
 
         action = thorium_message_action(&message);
 
+        CORE_DEBUGGER_ASSERT(action != ACTION_INVALID);
+
+        /*
+         * The worker always need metadata.
+         */
+        CORE_DEBUGGER_ASSERT(thorium_message_count(&message) > 0);
+
         /*
          * Give this message to the demultiplexer.
          */
@@ -862,7 +875,7 @@ int thorium_worker_dequeue_actor(struct thorium_worker *worker, struct thorium_a
          * The actor does not exist.
          */
         if (other_actor == NULL) {
-            thorium_worker_enqueue_message_for_triage(worker, &message);
+            thorium_worker_free_message(worker, &message);
 
             continue;
         }
@@ -1347,11 +1360,21 @@ void thorium_worker_free_message(struct thorium_worker *worker, struct thorium_m
 {
     int source_worker;
     void *buffer;
+    struct thorium_worker *sibling;
+    int injected_in_sibling;
+    int use_fast_path_for_dirty_message_injection;
+
+    use_fast_path_for_dirty_message_injection = YES;
 
     CORE_DEBUGGER_ASSERT_NOT_NULL(worker);
     CORE_DEBUGGER_ASSERT_NOT_NULL(message);
 
     buffer = thorium_message_buffer(message);
+
+    /*
+     * Messages that hit a worker can not have a NULL buffer.
+     */
+    CORE_DEBUGGER_ASSERT_NOT_NULL(buffer);
 
     /*
      * Nothing to do.
@@ -1364,7 +1387,8 @@ void thorium_worker_free_message(struct thorium_worker *worker, struct thorium_m
 
     if (source_worker == worker->name) {
 
-        /* This is from the current worker
+        /*
+         * This is from the current worker
          */
         core_memory_pool_free(&worker->outbound_message_memory_pool, buffer);
 #ifdef THORIUM_WORKER_DEBUG_INJECTION
@@ -1373,16 +1397,45 @@ void thorium_worker_free_message(struct thorium_worker *worker, struct thorium_m
 
     } else {
 
-        /* This is from another fellow local worker
+        /*
+         * This is from another fellow local worker
          * or from another BIOSAL node altogether.
          */
-
         CORE_DEBUGGER_ASSERT(thorium_message_buffer(message) != NULL);
-        thorium_worker_enqueue_message_for_triage(worker, message);
+
+        injected_in_sibling = NO;
+
+        /*
+         * Verify if the message has a marker.
+         */
+        if (use_fast_path_for_dirty_message_injection
+                        && source_worker != THORIUM_WORKER_NONE) {
+
+            CORE_DEBUGGER_ASSERT(source_worker >= 0);
+            CORE_DEBUGGER_ASSERT(source_worker < worker->worker_count);
+
+            /*
+            printf("DEBUG_INJECT_DIRTY injecting message for local worker %d\n", source_worker);
+            */
+
+            sibling = worker->workers + source_worker;
+
+            injected_in_sibling = thorium_worker_inject_clean_outbound_buffer(sibling, message);
+        }
+
+        /*
+         * Inject the message in the output ring. The message is either:
+         *
+         * - a node inbound message
+         * or
+         * - it is a worker outbound message and the input ring of this sibling worker is full.
+         */
+        if (!injected_in_sibling)
+            thorium_worker_enqueue_message_for_triage(worker, message);
     }
 }
 
-int thorium_worker_enqueue_message_for_triage(struct thorium_worker *worker, struct thorium_message *message)
+static int thorium_worker_enqueue_message_for_triage(struct thorium_worker *worker, struct thorium_message *message)
 {
 #ifdef THORIUM_WORKER_DEBUG_INJECTION
     int worker_name;
@@ -1637,7 +1690,7 @@ void thorium_worker_run(struct thorium_worker *worker)
     if (core_fast_queue_dequeue(&worker->output_message_queue_for_triage, &other_message)) {
 
         CORE_DEBUGGER_ASSERT(thorium_message_buffer(&other_message) != NULL);
-        thorium_worker_enqueue_message_for_triage(worker, &other_message);
+        thorium_worker_free_message(worker, &other_message);
     }
 
     /*
@@ -1715,7 +1768,7 @@ void thorium_worker_run(struct thorium_worker *worker)
              * It will be sent to its destination.
              */
 
-            thorium_worker_enqueue_message_for_triage(worker, &other_message);
+            thorium_worker_free_message(worker, &other_message);
 
         } else {
             /*
@@ -2008,8 +2061,8 @@ int thorium_worker_inject_clean_outbound_buffer(struct thorium_worker *self, voi
 {
     int value;
 
-    value = core_fast_ring_push_from_producer(&self->input_clean_outbound_buffer_ring,
-                    &buffer);
+    value = core_fast_ring_push_multiple_producers(&self->input_clean_outbound_buffer_ring,
+                    &buffer, self->name);
 
 #ifdef SHOW_FULL_RING_WARNINGS
     if (!value) {
@@ -2022,7 +2075,7 @@ int thorium_worker_inject_clean_outbound_buffer(struct thorium_worker *self, voi
 
 int thorium_worker_fetch_clean_outbound_buffer(struct thorium_worker *self, void **buffer)
 {
-    return core_fast_ring_pop_from_consumer(&self->input_clean_outbound_buffer_ring,
+    return core_fast_ring_pop_multiple_producers(&self->input_clean_outbound_buffer_ring,
                     buffer);
 }
 

engine/thorium/message.c
@@ -1,6 +1,9 @@
 
 #include "message.h"
 
+#include "actor.h" /* for THORIUM_ACTOR_NOBODY */
+#include "worker.h" /* for THORIUM_WORKER_NONE */
+
 #include <core/system/packer.h>
 #include <core/system/debugger.h>
 
@@ -22,19 +25,19 @@ void thorium_message_init(struct thorium_message *self, int action, int count,
     self->buffer = buffer;
     self->count = count;
 
-    self->source_actor = -1;
-    self->destination_actor = -1;
+    self->source_actor = THORIUM_ACTOR_NOBODY;
+    self->destination_actor = THORIUM_ACTOR_NOBODY;
 
     /* ranks are set with thorium_node_resolve */
-    self->source_node = -1;
-    self->destination_node = -1;
+    self->source_node = self->source_actor;
+    self->destination_node = self->destination_actor;
 
 #ifdef THORIUM_MESSAGE_USE_ROUTING
-    self->routing_source = -1;
-    self->routing_destination = -1;
+    self->routing_source = self->source_node;
+    self->routing_destination = self->destination_node;
 #endif
 
-    self->worker = -1;
+    self->worker = THORIUM_WORKER_NONE;
 
     thorium_message_set_type(self, THORIUM_MESSAGE_TYPE_NONE);
 
@@ -45,11 +48,11 @@ void thorium_message_init(struct thorium_message *self, int action, int count,
 
 void thorium_message_destroy(struct thorium_message *self)
 {
-    self->source_actor = -1;
-    self->destination_actor = -1;
-    self->action= -1;
+    self->source_actor = THORIUM_ACTOR_NOBODY;
+    self->destination_actor = THORIUM_ACTOR_NOBODY;
+    self->action= ACTION_INVALID;
     self->buffer = NULL;
-    self->count= 0;
+    self->count = 0;
 }
 
 int thorium_message_source(struct thorium_message *self)
@@ -213,11 +216,16 @@ int thorium_message_worker(struct thorium_message *self)
 void thorium_message_init_with_nodes(struct thorium_message *self, int count, void *buffer, int source,
                 int destination)
 {
-    int action;
+    /*
+     * Use a valid action to create the message, then switch it to
+     * a different value.
+     */
+    thorium_message_init(self, ACTION_PING, count, buffer);
 
-    action = -1;
-
-    thorium_message_init(self, action, count, buffer);
+    /*
+     * Set an invalid action specifier.
+     */
+    thorium_message_set_action(self, ACTION_INVALID);
 
     /*
      * Initially assign the MPI source rank and MPI destination

engine/thorium/worker.h
@@ -294,8 +294,6 @@ void thorium_worker_check_production(struct thorium_worker *self, int value, int
  */
 int thorium_worker_inject_clean_outbound_buffer(struct thorium_worker *self, void *buffer);
 int thorium_worker_fetch_clean_outbound_buffer(struct thorium_worker *self, void **buffer);
-int thorium_worker_enqueue_message_for_triage(struct thorium_worker *worker, struct thorium_message *message);
-int thorium_worker_dequeue_message_for_triage(struct thorium_worker *worker, struct thorium_message *message);
 
 void thorium_worker_print_balance(struct thorium_worker *self);
 int thorium_worker_spawn(struct thorium_worker *self, int script);

commit df6dd3c64935a03b18b9b8b1fbddd2de47889aa5

thorium: the dirty buffer ring does not store messages
The commit ab22c4fa5e89 ("thorium: increase actor-level
throughput by +75%") introduced a memory leak.

This commit fixes that regression.

Performance (without the memory leak):

PERFORMANCE_COUNTER computation-throughput = 1155988.011637 messages / s
PERFORMANCE_COUNTER actor-throughput = 412.852861 messages / s

Link: https://github.com/GeneAssembly/biosal/issues/751
Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

engine/thorium/worker.c
@@ -1420,7 +1420,8 @@ void thorium_worker_free_message(struct thorium_worker *worker, struct thorium_m
 
             sibling = worker->workers + source_worker;
 
-            injected_in_sibling = thorium_worker_inject_clean_outbound_buffer(sibling, message);
+            injected_in_sibling = thorium_worker_inject_clean_outbound_buffer(sibling,
+                            thorium_message_buffer(message));
         }
 
         /*