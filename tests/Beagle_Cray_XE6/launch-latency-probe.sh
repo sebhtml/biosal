48be5366af6818210ff4fd933c1c927d5dea8203
Sébastien Boisvert <boisvert@anl.gov>
2014-11-02 16:30:43 -0600

    test: use unique executable artefact for latency_probe test
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- tests/Beagle_Cray_XE6/launch-latency-probe.sh
+++ tests/Beagle_Cray_XE6/launch-latency-probe.sh
@@ -27,11 +27,7 @@ __COMMIT__=$(git log | head -n1 | awk '{print $2}')
 
 scripts/Cray_XE6/build-gnu.sh
 
-cd ..
-if ! test -e latency_probe
-then
-    ln -s biosal/performance/latency_probe/latency_probe
-fi
+cp biosal/performance/latency_probe/latency_probe $__JOB__.$__APP__
 
 cp biosal/tests/Beagle_Cray_XE6/Template.latency_probe.pbs $__JOB__.pbs
 
