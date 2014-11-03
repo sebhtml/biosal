3d18728ea9bc69971a63ae8f9a96f4a0e765abf7
Sébastien Boisvert <boisvert@anl.gov>
2014-11-03 12:03:06 -0500

    tests: add a test for deleting from a map
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

---
+++ tests/test_map_delete.c
@@ -0,0 +1,37 @@
+
+#include <core/structures/map.h>
+
+#include "test.h"
+
+#include <stdint.h>
+#include <inttypes.h>
+
+int main(int argc, char **argv)
+{
+    BEGIN_TESTS();
+
+    struct core_map container;
+    int key_size = sizeof(void *);
+    int value_size = sizeof(size_t);
+    uint64_t i;
+    uint64_t element_count;
+
+    element_count = 100000;
+
+    core_map_init(&container, key_size, value_size);
+
+    for (i = 0; i < element_count; ++i) {
+
+        TEST_POINTER_EQUALS(core_map_get(&container, &i), NULL);
+
+        core_map_add(&container, &i);
+
+        TEST_POINTER_NOT_EQUALS(core_map_get(&container, &i), NULL);
+    }
+
+    core_map_destroy(&container);
+
+    END_TESTS();
+
+    return 0;
+}
