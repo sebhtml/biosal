5b74c66ab47637304c2d77576edea4a3137cfa98
Sébastien Boisvert <boisvert@anl.gov>
2014-11-04 22:38:56 +0000

    tests: add launch script for latency_probe on Blue Gene/Q
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

---
+++ scripts/IBM_Blue_Gene_Q/build-xl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# http://www-01.ibm.com/support/docview.wss?uid=swg27022103&aid=1
+
+# -O5 to enable -ipa
+# -s to strip symbols
+
+CFLAGS="-I. -g -O3 -qmaxmem=-1 -qarch=qp -qtune=qp "
+CFLAGS="$CFLAGS -L/soft/libraries/alcf/current/xl/ZLIB/lib -I/soft/libraries/alcf/current/xl/ZLIB/include"
+
+make clean CONFIG_PAMI=y
+make -j 8 CFLAGS="$CFLAGS" CONFIG_PAMI=y CONFIG_DEBUG=y \
+    applications/spate_metagenome_assembler/spate \
+        performance/latency_probe/latency_probe
+
---
+++ tests/Cetus_IBM_Blue_Gene_Q/Template.latency_probe.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# echo "Commit= __COMMIT__"
+
+qsub \
+ --env PAMID_THREAD_MULTIPLE=1 \
+ -A CompBIO \
+ -n 1024 \
+ -t 01:00:00 \
+ -O __JOB__ \
+ --mode c1 \
+     __JOB__.__APP__ -print-load -threads-per-node 16
