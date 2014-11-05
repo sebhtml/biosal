sebhtml/biosal

commit 7c2dda5fb2c4397a10e7ca7f9b31e28d847a6b89

build: improve the option CONFIG_LTTNG
Move everything related to building with LTTng
in the build system itself.

Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

performance/tracepoints/Makefile.mk
@@ -3,7 +3,11 @@
 
 TRACEPOINTS_OBJECTS-y=
 
-#CONFIG_LTTNG=y
+CONFIG_LTTNG=n
+LTTNG_CFLAGS-$(CONFIG_LTTNG)=-DCONFIG_LDDNG
+LTTNG_LDFLAGS-$(CONFIG_LTTNG)=-llttng-ust -ldl
+CONFIG_CFLAGS += $(LTTNG_CFLAGS-y)
+CONFIG_LDFLAGS += $(LTTNG_LDFLAGS-y)
 
 TRACEPOINTS_OBJECTS-y += performance/tracepoints/message_tracepoints.o
 TRACEPOINTS_OBJECTS-y += performance/tracepoints/actor_tracepoints.o

scripts/lttng/build.sh
@@ -10,8 +10,7 @@ done
 #-std=c99
 #
 CFLAGS=" -O3 -march=x86-64 -g -I."
-CFLAGS+=" -D CONFIG_LTTNG"
-LDFLAGS=" -llttng-ust -ldl -lm -lz"
+
 clear
 echo "CFLAGS: $CFLAGS"
 

commit 9006075f72d8535846a7ec8341cb95e138272adb

tracing: fix LTTng option
Commit 7c2dda5fb2c4 introduced a regression in CONFIG_LTTNG.

Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

performance/tracepoints/Makefile.mk
@@ -4,7 +4,7 @@
 TRACEPOINTS_OBJECTS-y=
 
 CONFIG_LTTNG=n
-LTTNG_CFLAGS-$(CONFIG_LTTNG)=-DCONFIG_LDDNG
+LTTNG_CFLAGS-$(CONFIG_LTTNG)=-DCONFIG_LTTNG
 LTTNG_LDFLAGS-$(CONFIG_LTTNG)=-llttng-ust -ldl
 CONFIG_CFLAGS += $(LTTNG_CFLAGS-y)
 CONFIG_LDFLAGS += $(LTTNG_LDFLAGS-y)