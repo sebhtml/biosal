077e59eae5daa4e72364577dc94f23539ff5c7f9
Sébastien Boisvert <boisvert@anl.gov>
2014-11-06 14:02:22 -0500

    scripts: don't search for action specifiers defined with a BASE
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- scripts/check-tags.sh
+++ scripts/check-tags.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+echo "This does not include Thorium action specifiers, which are negative."
+
 echo "Tag names :"
-grep " ACTION_" * -R|grep define|grep -v check-tags|awk '{print $2}'|sort | uniq -c | grep " 1 " | wc -l
-grep " ACTION_" * -R|grep define|grep -v check-tags|awk '{print $2}'|sort | uniq -c | grep -v " 1 "
+grep " ACTION_" * -R|grep define|grep -v check-tags|grep -v _BASE|awk '{print $2}'|sort | uniq -c | grep " 1 " | wc -l
+grep " ACTION_" * -R|grep define|grep -v check-tags|grep -v _BASE|awk '{print $2}'|sort | uniq -c | grep -v " 1 "
 
 echo "Tag values :"
-grep " ACTION_" * -R|grep define|grep -v check-tags|awk '{print $3}'|sort | uniq -c | grep " 1 " | wc -l
-grep " ACTION_" * -R|grep define|grep -v check-tags|awk '{print $3}'|sort | uniq -c | grep -v " 1 "
+grep " ACTION_" * -R|grep define|grep -v check-tags|grep -v _BASE|awk '{print $3}'|sort | uniq -c | grep " 1 " | wc -l
+grep " ACTION_" * -R|grep define|grep -v check-tags|grep -v _BASE|awk '{print $3}'|sort | uniq -c | grep -v " 1 "
 
