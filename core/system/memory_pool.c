sebhtml/biosal

commit 153eea9475de41fad944bb3cfccd233091f8d344

thorium_node: use message type to select pool to free buffer
Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

core/system/memory_pool.c
@@ -317,30 +317,50 @@ void core_memory_pool_add_block(struct core_memory_pool *self)
     }
 }
 
-void core_memory_pool_free(struct core_memory_pool *self, void *pointer)
+int core_memory_pool_free(struct core_memory_pool *self, void *pointer)
 {
     size_t size;
+    int value;
 
     CORE_DEBUGGER_ASSERT(pointer != NULL);
 
     if (self == NULL) {
         core_memory_free(pointer, MEMORY_MEMORY_POOL);
-        return;
+        return 1;
     }
 
     size = 0;
-
-    core_memory_pool_free_private(self, pointer);
-
-    if (core_map_get_value(&self->allocated_blocks, &pointer, &size)) {
-
-        core_map_delete(&self->allocated_blocks, &pointer);
-    }
+    value = 0;
 
     /*
      * find out the actual size.
      */
+    if (core_map_get_value(&self->allocated_blocks, &pointer, &size)) {
+
+        core_memory_pool_free_private(self, pointer);
+
+        core_map_delete(&self->allocated_blocks, &pointer);
+/*
+        printf("DEBUG_MEMORY_POOL freed %zu bytes\n", size);
+        */
+
+        value = 1;
+    }
+
+#ifdef CORE_DEBUGGER_ENABLE_ASSERT2
+    if (!value)
+        printf("Error, memory pool 0x%x does not manage %p\n",
+                        self->name, pointer);
+
+    CORE_DEBUGGER_ASSERT(value);
+#endif
+
+    /*
+     * TODO: profile it only if it is found.
+     */
     core_memory_pool_profile(self, OPERATION_FREE, size);
+
+    return value;
 }
 
 void core_memory_pool_free_private(struct core_memory_pool *self, void *pointer)

core/system/memory_pool.h
@@ -70,7 +70,7 @@ void core_memory_pool_init(struct core_memory_pool *self, int block_size, int na
 void core_memory_pool_destroy(struct core_memory_pool *self);
 
 void *core_memory_pool_allocate(struct core_memory_pool *self, size_t size);
-void core_memory_pool_free(struct core_memory_pool *self, void *pointer);
+int core_memory_pool_free(struct core_memory_pool *self, void *pointer);
 
 void core_memory_pool_enable_tracking(struct core_memory_pool *self);
 void core_memory_pool_disable_tracking(struct core_memory_pool *self);

engine/thorium/message.c
@@ -93,15 +93,17 @@ void thorium_message_set_destination(struct thorium_message *self, int destinati
 void thorium_message_print(struct thorium_message *self)
 {
     printf("Message Number %d"
-                    " Action 0x%x Count %d SourceActor %d DestinationActor %d"
-                    " SourceNode %d DestinationNode %d\n",
+                    " Action 0x%x Count %d Buffer %p SourceActor %d DestinationActor %d"
+                    " SourceNode %d DestinationNode %d Type %d\n",
                     self->number,
                     self->action,
                     self->count,
+                    self->buffer,
                     self->source_actor,
                     self->destination_actor,
                     self->source_node,
-                    self->destination_node);
+                    self->destination_node,
+                    self->type);
 
     /*
     core_debugger_examine(self->buffer, self->count);

engine/thorium/node.c
@@ -1528,6 +1528,7 @@ struct thorium_actor *thorium_node_get_actor_from_name(struct thorium_node *node
 void thorium_node_dispatch_message(struct thorium_node *node, struct thorium_message *message)
 {
     void *buffer;
+    int type;
 
     tracepoint(thorium_node, dispatch_message_enter, node->name, node->tick,
                     thorium_message_destination(message));
@@ -1538,8 +1539,13 @@ void thorium_node_dispatch_message(struct thorium_node *node, struct thorium_mes
          * The buffer must be freed.
          */
         buffer = thorium_message_buffer(message);
+/*
+        thorium_message_print(message);
+        */
 
         if (buffer != NULL) {
+
+            type = thorium_message_type(message);
 
             /*
              * This is a Thorium core buffer since the workers are not authorized to talk to
@@ -1548,7 +1554,11 @@ void thorium_node_dispatch_message(struct thorium_node *node, struct thorium_mes
              * Also, this system buffer needs to be freed now anyway because there is no other
              * place where it will have the change to be freed.
              */
-            core_memory_pool_free(&node->inbound_message_memory_pool, buffer);
+
+            if (type == THORIUM_MESSAGE_TYPE_NODE_OUTBOUND)
+                core_memory_pool_free(&node->outbound_message_memory_pool, buffer);
+            else if (type == THORIUM_MESSAGE_TYPE_NODE_INBOUND)
+                core_memory_pool_free(&node->inbound_message_memory_pool, buffer);
         }
 
         return;
@@ -2630,9 +2640,10 @@ static void thorium_node_recycle_message(struct thorium_node *self, struct thori
      * Otherwise, free the buffer here directly since this is a Thorium core
      * buffer for startup.
      */
-    if (worker_name < 0) {
+    if (worker_name == THORIUM_WORKER_NONE) {
 
         core_memory_pool_free(&self->inbound_message_memory_pool, buffer);
+
 #ifdef THORIUM_NODE_DEBUG_INJECTION
         ++self->counter_freed_injected_node_inbound_buffers;
 #endif

performance/latency_probe/process.c
@@ -201,7 +201,6 @@ static void process_receive(struct thorium_actor *self, struct thorium_message *
 
             core_timer_init(&concrete_self->timer);
             core_timer_start(&concrete_self->timer);
-
         }
     } else if (action == ACTION_NOTIFY) {
 

commit df97d4a745d8d2696fb786646a0d995c97b9942d

core: fix memory leak in core_memory_pool for large segments
Large segments (not fulfilled by memory blocks) are never freed
when the flag FLAG_ENABLE_TRACKING is cleared.

This bug was introduced in commit 153eea9 ("thorium_node: use
message type to select pool to free buffer").

This patch adds a fallback code path for large segments when
FLAG_ENABLE_TRACKING is cleared.

Link: https://github.com/GeneAssembly/biosal/issues/788
Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

core/system/memory_pool.c
@@ -448,6 +448,34 @@ int core_memory_pool_free(struct core_memory_pool *self, void *pointer)
 #endif
 
     /*
+     * If the FLAG_ENABLE_TRACKING bit is 0, then the map "allocated_blocks"
+     * never contains anything (even for large segments). Thus, the if block
+     * just above this comment never does anything when FLAG_ENABLE_TRACKING
+     * is 0.
+     *
+     * The solution, when FLAG_ENABLE_TRACKING is cleared, is to send every
+     * allocation through the core_memory_pool_free_private() code path. In
+     * this function, large_blocks are still tracked even if
+     * FLAG_ENABLE_TRACKING is 0.
+     *
+     * This is to avoid errors when memory blocks of the memory pool can not
+     * fulfill allocations that are too large.
+     *
+     * This memory leak was affecting performance as well, when the ephemeral
+     * memory pool of any worker allocates large segments of memory because
+     * these were not freed anymore.
+     *
+     * The call to core_memory_pool_free_private() below call fixes the
+     * regression added in commit 153eea9 ("thorium_node: use message type to
+     * select pool to free buffer").
+     *
+     * Link: https://github.com/GeneAssembly/biosal/issues/788#issuecomment-62002496
+     */
+    if (!CORE_BITMAP_GET_BIT(self->flags, FLAG_ENABLE_TRACKING)) {
+        core_memory_pool_free_private(self, pointer);
+    }
+
+    /*
      * Profile the call. This is done even when FLAG_ENABLE_TRACKING
      * is not set.
      */