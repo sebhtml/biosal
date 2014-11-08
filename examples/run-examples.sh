24bed909b0d67855db9d12db5e1005970d4a077d
Sébastien Boisvert <boisvert@anl.gov>
2014-11-08 12:00:55 -0500

    tests: add time to example tests
    Link: https://github.com/GeneAssembly/biosal/issues/767
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- examples/run-examples.sh
+++ examples/run-examples.sh
@@ -7,15 +7,17 @@ function run_example()
     local example
     local result
     local load_lines
+    local time_value
 
     example=$1
 
-    echo -n "ExampleTest $example "
+    echo -n "ExampleTest $example"
 
     (
     time make $example
     ) &> $example.log
 
+    time_value=$(tail -n3 $example.log|head -n1|awk '{print $2}')
     load_lines=$(grep "COMPUTATION LOAD" $example.log | grep node | wc -l)
     result="FAILED"
 
@@ -23,7 +25,7 @@ function run_example()
     then
         result="PASSED"
     fi
-    echo " result: $result (see $example.log)"
+    echo " result: $result time: $time_value (see $example.log)"
 }
 
 function main()
