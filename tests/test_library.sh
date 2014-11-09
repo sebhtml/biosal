db6b36e1f38c7ca3db61c09fe5735e2aae134918
Sébastien Boisvert <boisvert@anl.gov>
2014-11-09 11:17:44 -0500

    tests: convert time from the format 0m0.012s to a number of seconds
    Link: https://github.com/GeneAssembly/biosal/issues/802
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- tests/test_library.sh
+++ tests/test_library.sh
@@ -3,6 +3,58 @@
 #maximum_test_count=1
 maximum_test_count=9999
 
+function biosal_filter_time_value()
+{
+    local time_value
+    local seconds
+    local minutes
+    local hours
+    local token_count
+    local new_time_value
+
+    time_value=$1
+
+    if test $(echo $time_value | grep "s" | wc -l) -gt 0
+    then
+        new_time_value=$(echo $time_value|sed 's/h/ /g'|sed 's/m/ /g'|sed 's/s//g')
+
+        hours=0
+        minutes=0
+        seconds=0
+        token_count=$(echo "$new_time_value" | wc -w)
+
+        #echo "token_count $token_count"
+
+        # only seconds
+        if test $token_count -eq 1
+        then
+            # nothing to do
+            seconds=$new_time_value
+
+        # minutes and seconds
+        elif test $token_count -eq 2
+        then
+            seconds=$(echo "$new_time_value" | awk '{print $2}')
+            minutes=$(echo "$new_time_value" | awk '{print $1}')
+
+        # hours, minutes, seconds
+        elif test $token_count -eq 3
+        then
+            seconds=$(echo "$new_time_value" | awk '{print $3}')
+            minutes=$(echo "$new_time_value" | awk '{print $2}')
+            hours=$(echo "$new_time_value" | awk '{print $1}')
+        fi
+
+        # remove decimal point
+        seconds=$(echo $seconds | sed 's/\./ /g'|awk '{print $1}')
+
+        #echo "hours $hours minutes $minutes seconds $seconds"
+        time_value=$(($hours * 60 * 60 + $minutes *  60 + $seconds))
+    fi
+
+    echo $time_value
+}
+
 function biosal_shell_summarize_test_result()
 {
     local title
@@ -76,6 +128,8 @@ function biosal_test_junit_emit_testcase()
     name=$2
     time_value=$3
     error=$4
+
+    time_value=$(biosal_filter_time_value $time_value)
 
     echo "        <testcase classname=\"$classname\" name=\"$name\" time=\"$time_value\">"
 
