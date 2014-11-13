fa7299bbfeea4dbd56f68f77264acf8f038cd663
George K. Thiruvathukal <gkt@cs.luc.edu>
2014-11-13 09:25:03 -0600

    examples: create first version of distributed Hello, World
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- examples/hello_acquaintances/hello_acq.c
+++ examples/hello_acquaintances/hello_acq.c
@@ -1,3 +1,6 @@
+/*
+ * hello_acq.c: Distributed Hello, World. Say hi to yourself and to your peers (first version).
+ */
 
 #include "hello_acq.h"
 
@@ -47,17 +50,32 @@ void hello_acq_receive(struct thorium_actor *actor, struct thorium_message *mess
     buffer = thorium_message_buffer(message);
 
     if (tag == ACTION_START) {
-
         core_vector_unpack(&hello1->initial_helloes, buffer);
 
         printf("Hello world ! my name is actor:%d and I have %d acquaintances:",
                         name, (int)core_vector_size(&hello1->initial_helloes));
 
+        /* This is just to print the acquaintances as in the original hello.c version. */
         for (i = 0; i < core_vector_size(&hello1->initial_helloes); i++) {
             printf(" actor:%d", core_vector_at_as_int(&hello1->initial_helloes, i));
+            printf("\n");
         }
-        printf("\n");
 
+        /* This is to send messages to myself and my peers. */
+        for (i = 0; i < core_vector_size(&hello1->initial_helloes); i++) {
+            if (name == core_vector_at_as_int(&hello1->initial_helloes, i)) {
+                thorium_actor_send_to_self_empty(actor, ACTION_HELLO_ACQ_SELF);
+            } else {
+            	thorium_actor_send_int(actor, core_vector_at_as_int(&hello1->initial_helloes, i), ACTION_HELLO_ACQ_PEER, name);
+            }
+        }
+
+    } else if (tag == ACTION_HELLO_ACQ_SELF) {
+    	int source = thorium_message_source(message);
+    	printf("Hello received from me (%d) to myself (%d)\n", source, name);
+    } else if (tag == ACTION_HELLO_ACQ_PEER) {
+    	int source = thorium_message_source(message);
+    	printf("Hello received by %d from peer %d\n", name, source);
         thorium_actor_send_to_self_empty(actor, ACTION_STOP);
     }
 }
--- examples/hello_acquaintances/hello_acq.h
+++ examples/hello_acquaintances/hello_acq.h
@@ -10,6 +10,11 @@ struct hello_acq {
     struct core_vector initial_helloes;
 };
 
+#define HELLO_ACQ_ACTION_BASE -13000
+
+#define ACTION_HELLO_ACQ_PEER (HELLO_ACQ_ACTION_BASE + 0)
+#define ACTION_HELLO_ACQ_SELF (HELLO_ACQ_ACTION_BASE + 1)
+
 extern struct thorium_script hello_acq_script;
 
 #endif
