fc7e6b1e138cfb3e4256654a304af167df5c0861
Sébastien Boisvert <boisvert@anl.gov>
2014-11-14 15:25:31 -0500

    core: fix compaction bug in queue
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- core/structures/queue.c
+++ core/structures/queue.c
@@ -12,8 +12,10 @@
 
 
 /*
-#define CORE_QUEUE_DEBUG
 */
+#define CORE_QUEUE_DEBUG_COMPACT
+
+void core_queue_compact(struct core_queue *self);
 
 void core_queue_init(struct core_queue *self, int bytes_per_unit)
 {
@@ -37,13 +39,7 @@ void core_queue_destroy(struct core_queue *self)
 
 int core_queue_enqueue(struct core_queue *self, void *item)
 {
-    int stride;
-    int i;
     void *bucket1;
-    void *bucket2;
-    int offset1;
-    int offset2;
-    int minimum_stride;
 
 #ifdef CORE_QUEUE_DEBUG
     int value;
@@ -53,52 +49,11 @@ int core_queue_enqueue(struct core_queue *self, void *item)
     printf("DEBUG enqueue %d, size %d\n", value, self->size);
 #endif
 
-    /* The vector is full, try to move things on the left.
+    core_queue_compact(self);
+
+    /* There was no place, append the item in the vector
      */
     if (self->enqueue_index == core_vector_size(&self->vector)) {
-        stride = self->dequeue_index;
-
-#ifdef CORE_QUEUE_DEBUG
-        printf("dequeue_index %d\n", self->dequeue_index);
-#endif
-
-        minimum_stride = core_vector_size(&self->vector) / 8;
-
-        /* move things on the left
-         */
-        if (stride > 0 && self->size > 0
-                        && stride >= minimum_stride) {
-
-            i = 0;
-            while (i < self->size) {
-                offset1 = self->dequeue_index + i;
-                offset2 = self->dequeue_index + i - stride;
-
-                bucket1 = core_vector_at(&self->vector, offset1);
-                bucket2 = core_vector_at(&self->vector, offset2);
-
-#ifdef CORE_QUEUE_DEBUG
-                printf("DEBUG moving %d %p to %d %p (size %d, vector size %d)\n", offset1, bucket1, offset2, bucket2,
-                                self->size, (int)core_vector_size(&self->vector));
-#endif
-
-                core_memory_copy(bucket2, bucket1, self->bytes_per_element);
-
-                i++;
-            }
-
-            self->enqueue_index -= stride;
-            self->dequeue_index -= stride;
-
-            bucket1 = core_vector_at(&self->vector, self->enqueue_index);
-            core_memory_copy(bucket1, item, self->bytes_per_element);
-            self->enqueue_index++;
-            self->size++;
-            return 1;
-        }
-
-        /* there was no place, append the item in the vector
-         */
 
         core_vector_push_back(&self->vector, item);
         self->enqueue_index++;
@@ -109,7 +64,6 @@ int core_queue_enqueue(struct core_queue *self, void *item)
 
     /* at this point, there is plenty of space...
      */
-
     bucket1 = core_vector_at(&self->vector, self->enqueue_index);
     core_memory_copy(bucket1, item, self->bytes_per_element);
     self->enqueue_index++;
@@ -121,6 +75,7 @@ int core_queue_enqueue(struct core_queue *self, void *item)
 int core_queue_dequeue(struct core_queue *self, void *item)
 {
     void *bucket;
+
     if (self->size == 0) {
         return 0;
     }
@@ -129,6 +84,8 @@ int core_queue_dequeue(struct core_queue *self, void *item)
     core_memory_copy(item, bucket, self->bytes_per_element);
     self->dequeue_index++;
     self->size--;
+
+    core_queue_compact(self);
 
     return 1;
 }
@@ -156,11 +113,51 @@ void core_queue_set_memory_pool(struct core_queue *self,
 
 void core_queue_print(struct core_queue *self)
 {
-    printf("core_queue_print capacity is %" PRId64 "\n",
+    printf("core_queue_print size %d dequeue_index %d enqueue_index %d vector_capacity %" PRId64 "\n",
+                    self->size, self->dequeue_index, self->enqueue_index,
                     core_vector_capacity(&self->vector));
 }
 
 int core_queue_capacity(struct core_queue *self)
 {
     return core_vector_capacity(&self->vector);
+}
+
+void core_queue_compact(struct core_queue *self)
+{
+    int available;
+    int minimum;
+    int i;
+    int index;
+    void *source;
+    void *destination;
+
+    available = self->dequeue_index;
+    minimum = 64;
+
+    /*
+    printf("core_queue_compact size %d dequeue_index %d enqueue_index %d\n",
+                    self->size, self->dequeue_index, self->enqueue_index);
+                    */
+
+    if (available < minimum)
+        return;
+
+    i = 0;
+    /*
+     * move all items.
+     */
+    while (i < self->size) {
+        index = self->dequeue_index + i;
+
+        source = core_vector_at(&self->vector, index);
+        destination = core_vector_at(&self->vector, i);
+
+        core_memory_copy(destination, source, self->bytes_per_element);
+
+        ++i;
+    }
+
+    self->dequeue_index -= available;
+    self->enqueue_index -= available;
 }
