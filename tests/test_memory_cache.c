7d326ca0501f6afe35afb5c6cdce30ebd5d7fbec
Sébastien Boisvert <boisvert@anl.gov>
2014-11-20 16:45:38 -0500

    tests: add more tests for memory_cache structure
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- tests/test_memory_cache.c
+++ tests/test_memory_cache.c
@@ -3,6 +3,8 @@
 
 #include <core/system/memory_cache.h>
 
+#include <core/structures/vector.h>
+
 #include <stdint.h>
 #include <inttypes.h>
 
@@ -15,6 +17,8 @@ int main(int argc, char **argv)
     size_t chunk_size = 8388608;
     void *pointer;
     void *old_pointer;
+    struct core_vector vector;
+    int i;
 
     core_memory_cache_init(&cache, -1, size, chunk_size);
 
@@ -31,8 +35,21 @@ int main(int argc, char **argv)
 
     TEST_POINTER_EQUALS(pointer, old_pointer);
 
+    core_memory_cache_free(&cache, pointer);
+    TEST_INT_EQUALS(core_memory_cache_balance(&cache), 0);
+
+    core_vector_init(&vector, sizeof(void *));
+
+    for (i = 0; i < 2000000; ++i) {
+
+        pointer = core_memory_cache_allocate(&cache, size);
+        TEST_POINTER_NOT_EQUALS(pointer, NULL);
+
+        core_vector_push_back(&vector, &pointer);
+    }
     core_memory_cache_destroy(&cache);
 
+    core_vector_destroy(&vector);
     END_TESTS();
 
     return 0;
