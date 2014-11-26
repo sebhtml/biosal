7e2c1306a458d560f87658ba42a7a19f93582f33
George K. Thiruvathukal <gkt@cs.luc.edu>
2014-11-25 21:30:33 -0600

    hello_acq: Revise code to have clear causality chain (tested on mpirun -np 1..16)
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- examples/hello_acquaintances/hello_acq.h
+++ examples/hello_acquaintances/hello_acq.h
@@ -17,6 +17,7 @@ struct hello_acq {
 #define ACTION_HELLO_ACQ_SELF (HELLO_ACQ_ACTION_BASE + 1)
 #define ACTION_HELLO_GREET_OTHERS (HELLO_ACQ_ACTION_BASE + 2)
 #define ACTION_HELLO_DONE_GREETING_ALL (HELLO_ACQ_ACTION_BASE + 3)
+#define ACTION_HELLO_PEER_DONE (HELLO_ACQ_ACTION_BASE + 4)
 
 extern struct thorium_script hello_acq_script;
 
