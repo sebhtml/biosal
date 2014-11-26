284616f390ff3b2c38914291f7e5a5ef8d4a11c3
Sébastien Boisvert <boisvert@anl.gov>
2014-11-25 23:21:03 -0500

    teests: add more tests for the binary heap
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- tests/test_binary_heap.c
+++ tests/test_binary_heap.c
@@ -14,6 +14,12 @@
 int main(int argc, char **argv)
 {
     struct core_binary_heap heap;
+    int key;
+    uint64_t value;
+    int *key_pointer;
+    uint64_t *value_pointer;
+    int i;
+    int count;
 
     BEGIN_TESTS();
 
@@ -21,6 +27,25 @@ int main(int argc, char **argv)
                     CORE_BINARY_HEAP_MIN | CORE_BINARY_HEAP_INT_KEYS);
 
     TEST_INT_EQUALS(core_binary_heap_size(&heap), 0);
+
+    count = 10000;
+    i = 0;
+
+    while (i < count) {
+        key = 9 + i;
+        value = 1234 * key;
+        ++i;
+
+        core_binary_heap_insert(&heap, &key, &value);
+
+        TEST_INT_EQUALS(core_binary_heap_size(&heap), i);
+
+        TEST_BOOLEAN_EQUALS(core_binary_heap_get_root(&heap, (void **)&key_pointer,
+                                (void **)&value_pointer), TRUE);
+
+        TEST_INT_EQUALS(*key_pointer, key);
+        TEST_INT_EQUALS(*value_pointer, value);
+    }
 
     core_binary_heap_destroy(&heap);
 
