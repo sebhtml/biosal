d0b5c89e7dd55ad5c1f683f98b9849270e6b73a9
Sébastien Boisvert <boisvert@anl.gov>
2014-12-10 15:21:03 -0500

    core: use the word flag instead of bit in bitmap macros
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- core/helpers/bitmap.h
+++ core/helpers/bitmap.h
@@ -9,9 +9,6 @@
 
 #define CORE_BITS_PER_BYTE 8
 
-#define CORE_BITMAP_MAKE_FLAG(i) \
-        (1 << i)
-
 int core_bitmap_get_bit_uint8_t(uint8_t *self, int index);
 void core_bitmap_set_bit_value_uint8_t(uint8_t *self, int index, int value);
 
@@ -24,16 +21,19 @@ void core_bitmap_clear_bit_uint32_t(uint32_t *self, int index);
 int core_bitmap_get_bit_uint64_t(uint64_t *self, int index);
 void core_bitmap_set_bit_value_uint64_t(uint64_t *self, int index, int value);
 
+#define CORE_BITMAP_MAKE_FLAG(i) \
+        (1 << i)
+
 #define CORE_BITMAP_CLEAR_FLAGS(bitmap) \
         bitmap = 0;
 
-#define CORE_BITMAP_GET_FLAG(bitmap, bit) \
-        (bitmap & (bit))
+#define CORE_BITMAP_GET_FLAG(bitmap, flag) \
+        (bitmap & (flag))
 
-#define CORE_BITMAP_SET_FLAG(bitmap, bit) \
-        bitmap = (bitmap | (bit))
+#define CORE_BITMAP_SET_FLAG(bitmap, flag) \
+        bitmap = (bitmap | (flag))
 
-#define CORE_BITMAP_CLEAR_FLAG(bitmap, bit) \
-        bitmap = (bitmap & (~(bit)))
+#define CORE_BITMAP_CLEAR_FLAG(bitmap, flag) \
+        bitmap = (bitmap & (~(flag)))
 
 #endif
