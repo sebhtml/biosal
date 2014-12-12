cad1f8069b12025b8d2afc34246b8ed644b6ebb8
Sébastien Boisvert <boisvert@anl.gov>
2014-12-12 11:48:03 -0500

    tests: add spate launcher for Mira
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

---
+++ tests/Mira_IBM_Blue_Gene_Q/Template.spate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# echo "Commit= __COMMIT__"
+
+# -print-thorium-data
+
+qsub \
+ --env PAMID_THREAD_MULTIPLE=1 \
+ -A CompBIO \
+ -n 1024 \
+ -t 03:00:00 \
+ -O __JOB__ \
+ --mode c1 \
+     __JOB__.__APP__ -threads-per-node 16 \
+    -k 27 __SAMPLE__/*.fastq \
+    -minimum-coverage 2 \
+    -print-thorium-data \
+    -o __JOB__ \
