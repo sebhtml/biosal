d1da98905a283fa72da33700c172017d8877494f
Sébastien Boisvert <boisvert@anl.gov>
2014-12-13 21:04:59 +0000

    tests: use nodes and threads in job template for latency_probe on Mira
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- tests/Mira_IBM_Blue_Gene_Q/Template.latency_probe.sh
+++ tests/Mira_IBM_Blue_Gene_Q/Template.latency_probe.sh
@@ -5,8 +5,8 @@
 qsub \
  --env PAMID_THREAD_MULTIPLE=1 \
  -A CompBIO \
- -n 1024 \
+ -n __NODES__ \
  -t __WALLTIME__ \
  -O __JOB__ \
  --mode c1 \
-     __JOB__.__APP__ -print-thorium-data -threads-per-node 16
+     __JOB__.__APP__ -print-thorium-data -threads-per-node __THREADS__
