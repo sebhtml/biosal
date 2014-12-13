3208cd2c663abd8093ef1e02be3d4472b1ec5375
Sébastien Boisvert <boisvert@anl.gov>
2014-12-13 18:39:02 +0000

    tests: lower walltime to 30 minutes for latency_probe on Mira
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- tests/Mira_IBM_Blue_Gene_Q/launch-latency_probe.sh
+++ tests/Mira_IBM_Blue_Gene_Q/launch-latency_probe.sh
@@ -4,7 +4,7 @@ center=alcf
 system=mira
 nodes=512
 threads=16
-walltime=01:00:00
+walltime=00:30:00
 
 build_script=scripts/IBM_Blue_Gene_Q/build-xl.sh
 app_path=performance/latency_probe/latency_probe
