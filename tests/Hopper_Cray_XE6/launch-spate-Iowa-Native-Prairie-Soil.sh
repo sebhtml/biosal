c2f2deb25401a94913bb9ffbe8b6c956f369c9ae
Sébastien Boisvert <boisvert@anl.gov>
2014-12-13 18:13:43 -0800

    tests: fix Hopper Spate launcher
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- tests/Hopper_Cray_XE6/launch-spate-Iowa-Native-Prairie-Soil.sh
+++ tests/Hopper_Cray_XE6/launch-spate-Iowa-Native-Prairie-Soil.sh
@@ -7,8 +7,8 @@ threads=24
 walltime=03:00:00
 
 build_script=scripts/Cray_XE6/build-script-hopper.sh
-app_path=applications/spate_metagenome_assembler/spateaaa
-job_template=biosal/tests/Hopper_Cray_XE6/Template.$app.pbs
+app_path=applications/spate_metagenome_assembler/spate
+job_template=biosal/tests/Hopper_Cray_XE6/Template.spate.pbs
 
 root=/project/projectdirs/m1523/Jobs-Hopper
 dataset=/scratch/scratchdirs/boisvert/Iowa_Native_Prairie_Soil
