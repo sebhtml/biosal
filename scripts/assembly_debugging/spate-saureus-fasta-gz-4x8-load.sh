1496a9d6e632d95d2111b30bece761cd83de4524
Sébastien Boisvert <boisvert@anl.gov>
2014-12-14 17:43:58 -0500

    scripts: remove the use of -freopen-stdout
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- scripts/assembly_debugging/spate-saureus-fasta-gz-4x8-load.sh
+++ scripts/assembly_debugging/spate-saureus-fasta-gz-4x8-load.sh
@@ -4,4 +4,4 @@
 #-enable-actor-load-profiler -enable-transport-profiler
 
 mpiexec -n 4 ./applications/spate_metagenome_assembler/spate -k 33 -threads-per-node 8 ~/dropbox/S.aureus.fasta.gz \
-    -print-thorium-data -freopen-stdout -o spate-007
+    -print-thorium-data
