c26714e5b338b5caf51407e3edbc15bce48cfed4
Sébastien Boisvert <boisvert@anl.gov>
2014-12-14 05:48:20 +0000

    scripts: add script to build latency_probe
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

---
+++ scripts/IBM_Blue_Gene_Q/build-xl-latency_probe.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# http://www-01.ibm.com/support/docview.wss?uid=swg27022103&aid=1
+
+# -O5 to enable -ipa
+# -s to strip symbols
+
+CFLAGS="-I. -g -O3 -qmaxmem=-1 -qarch=qp -qtune=qp "
+CFLAGS="$CFLAGS -L/soft/libraries/alcf/current/xl/ZLIB/lib -I/soft/libraries/alcf/current/xl/ZLIB/include"
+
+make clean CONFIG_PAMI=y
+make -j 8 CFLAGS="$CFLAGS" CONFIG_PAMI=y CONFIG_DEBUG=n \
+        performance/latency_probe/latency_probe
+
