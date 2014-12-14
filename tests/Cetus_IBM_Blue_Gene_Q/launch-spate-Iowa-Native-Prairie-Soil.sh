b1e56a8a1614a0ea8a374a39f59dd93c9aaf4e2c
Sébastien Boisvert <boisvert@anl.gov>
2014-12-14 05:48:58 +0000

    tests: simplify spate launcher for cetus
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

---
+++ tests/Cetus_IBM_Blue_Gene_Q/launch-spate-Iowa-Native-Prairie-Soil.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+center=alcf
+system=cetus
+nodes=512
+threads=16
+walltime=01:00:00
+
+build_script=scripts/IBM_Blue_Gene_Q/build-spate-xl.sh
+app_path=applications/spate_metagenome_assembler/spate
+job_template=tests/Mira_IBM_Blue_Gene_Q/Template.spate.sh
+
+root=/projects/CompBIO/Projects/automated-tests-mira
+dataset=/gpfs/mira-fs1/projects/CompBIO/Datasets/JGI/Great_Prairie_Soil_Metagenome_Grand_Challenge/Datasets/Iowa_Native_Prairie_Soil
+
+submit_command=bash
+
+source tests/submit-actor-job-to-scheduler.sh
+
