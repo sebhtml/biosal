8b6a31719af18eedd54f378e21a9d1715ea1e4ea
Sébastien Boisvert <boisvert@anl.gov>
2014-12-14 05:51:58 +0000

    tests: fix cetus root
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- tests/Cetus_IBM_Blue_Gene_Q/launch-spate-Iowa-Native-Prairie-Soil.sh
+++ tests/Cetus_IBM_Blue_Gene_Q/launch-spate-Iowa-Native-Prairie-Soil.sh
@@ -10,7 +10,7 @@ build_script=scripts/IBM_Blue_Gene_Q/build-spate-xl.sh
 app_path=applications/spate_metagenome_assembler/spate
 job_template=tests/Mira_IBM_Blue_Gene_Q/Template.spate.sh
 
-root=/projects/CompBIO/Projects/automated-tests-mira
+root=/projects/CompBIO/Projects/automated-tests-cetus
 dataset=/gpfs/mira-fs1/projects/CompBIO/Datasets/JGI/Great_Prairie_Soil_Metagenome_Grand_Challenge/Datasets/Iowa_Native_Prairie_Soil
 
 submit_command=bash
