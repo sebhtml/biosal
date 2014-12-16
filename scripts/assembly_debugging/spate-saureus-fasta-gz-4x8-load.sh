668932b8db3f945fedf2c68dede05a0a790a2d0d
Sébastien Boisvert <boisvert@anl.gov>
2014-12-16 18:13:19 -0500

    scripts: enable actor log information
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- scripts/assembly_debugging/spate-saureus-fasta-gz-4x8-load.sh
+++ scripts/assembly_debugging/spate-saureus-fasta-gz-4x8-load.sh
@@ -4,4 +4,4 @@
 #-enable-actor-load-profiler -enable-transport-profiler
 
 mpiexec -n 4 ./applications/spate_metagenome_assembler/spate -k 33 -threads-per-node 8 ~/dropbox/S.aureus.fasta.gz \
-    -print-thorium-data
+    -print-thorium-data -enable-actor-log all \
