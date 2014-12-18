a97e2800225e785aa1ade474bf02ccdb46659671
Sébastien Boisvert <boisvert@anl.gov>
2014-12-17 21:04:33 -0500

    scripts: test option -degree-of-aggregation-limit
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- scripts/performance/latency-probe-4x8.sh
+++ scripts/performance/latency-probe-4x8.sh
@@ -2,4 +2,5 @@
 
 mpiexec -n 4 performance/latency_probe/latency_probe -threads-per-node 8 \
     -print-thorium-data \
-    -ping-event-count-per-actor 100000 \
+    -ping-event-count-per-actor 20000 \
+    -degree-of-aggregation-limit 20 \
