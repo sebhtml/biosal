dc9c879c8bb7562808aa1fb30d6e845dcfb5f39d
Sébastien Boisvert <boisvert@anl.gov>
2015-03-31 17:16:53 -0400

    integer: improve header
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- core/helpers/integer.h
+++ core/helpers/integer.h
@@ -2,8 +2,9 @@
 #ifndef INTEGER_HEADER
 #define INTEGER_HEADER
 
+
 int core_int_pack_size(int *self);
 int core_int_pack(int *self, char *buffer);
 int core_int_unpack(int *self, char *buffer);
 
-#endif
+#endif /* INTEGER_HEADER */
