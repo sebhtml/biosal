fc3190b1eabc09a1fab45646251bd218a7293d45
Sébastien Boisvert <boisvert@anl.gov>
2015-03-31 20:08:59 -0400

    scripts: improve demo script
    Signed-off-by: Sébastien Boisvert <boisvert@anl.gov>

--- scripts/laptop-workload/run-on-short-velvet-data.sh
+++ scripts/laptop-workload/run-on-short-velvet-data.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+cores=$(cat /proc/cpuinfo|grep "model name"|wc -l)
+
+cores=$(($cores / 2))
+
+if test $cores -lt 1
+then
+    cores=1
+fi
+
+cores=2
+
 make -j applications/spate_metagenome_assembler/spate
 
 if ! test -d velvet
@@ -12,11 +23,11 @@ then
     rm -rf spate_output
 fi
 
-mpiexec -n 1 applications/spate_metagenome_assembler/spate -k 25 -threads-per-node 2 \
+mpiexec -n 1 applications/spate_metagenome_assembler/spate -k 25 -threads-per-node $cores \
     velvet/data/test_reads.fa \
     -o spate_output \
     -enable-actor-log "biosal_tip_manager" \
+   -enable-actor-log all \
 
 exit
 
-    -enable-actor-log all \
